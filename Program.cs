b8dca9b38d047b2ab8f39c48c113945727509844
arthur_kamau <kamaukenn11@gmail.com>
2020-10-09 19:35:26 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+// using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace StratosServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            var host = new WebHostBuilder()
+              .UseKestrel()
+              .UseStartup<Startup>()
+              .ConfigureKestrel((context, options) =>
+                    {
+                        options.AllowSynchronousIO = true;
+                    })
+              .UseIISIntegration()
+              .UseContentRoot(Directory.GetCurrentDirectory())
+              .UseUrls("http://localhost:3000")
+              .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                })
+              .Build();
+            host.Run();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Kestral
-C# Kestral server
+## Kestral
+Using c# Kestrel server to create a simple sample of a language server
+
+### Description
+simple language server api. 
+
+### Command template
+`/tools/antlr4-csharp-4.6.1-SNAPSHOT-complete.jar -package <NAMESPACE-OF-YOUR-PROGRAM> -o <OUTPUT_DIR> -Dlanguage=CSharp_v4_5 <PATH-TO-GRAMMAR>`
+
+### Command to generate c# Files 
+`cd antlr && java -jar   antlr-4.8-complete.jar  -visitor  -package StratosServer -o ../generated/ -Dlanguage=CSharp StratosParser.g4 StratosLexer.g4 && cd ..`
